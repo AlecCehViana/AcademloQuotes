4d7bfa693446b1a8ee7376095bfa9d9a344a07a5
AlecCehVina <aleccehviana@gmail.com>
2022-06-27 15:43:08 -0500

    FirstCommit

---
+++ src/components/Presentation.js
@@ -0,0 +1,21 @@
+import React from "react"
+const Presentation = () => {
+   
+        
+
+    
+   
+
+
+ return (
+     <div  className="my-name">
+         
+        <h1> Alec Ceh Viana</h1>
+    
+       
+     
+     </div>
+ )
+}
+
+export default Presentation
---
+++ src/components/QuotesBox.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import quotes from '../quotes.json'
+ 
+
+const colors = ["#240046", "#8d0801", "#005b00", "#ff7b00", "#990033", "#140152"]
+
+const QuoteBox = () => {
+    let RandomNumber =Math.floor(Math.random()*quotes.length);
+    const [ index, setIndex] = useState(RandomNumber)
+   
+    const changeQuote = () => {
+        let RandomNumber =Math.floor(Math.random()*quotes.length);
+        setIndex(RandomNumber)
+        
+
+    }
+    const randomColor = Math.floor(Math.random() * colors.length )
+    document.body.style = `background: ${colors[randomColor]}`
+
+ console.log(quotes)
+ return (
+     <div className="containerBox" style={{color: colors[randomColor] }}>
+        
+        <div className="quote-design"> <h2>"{quotes[index].quote}"</h2>  </div>
+
+        <div className="author-design"> <h4> — {quotes[index].author}</h4></div>
+      
+        <button onClick={changeQuote} style={{backgroundColor: colors[randomColor] }}> <i class="fa-solid fa-shuffle"></i></button>
+     
+     </div>
+ )
+}
+export default QuoteBox
